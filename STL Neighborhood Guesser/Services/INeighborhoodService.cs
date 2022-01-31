9030707f7fb8e340e4bc7b4da4a44c8e550d5cb8
fozzymo2 <danielcslattery@gmail.com>
2022-01-31 11:43:55 -0600

    Seperated location of INeighborhoodSerivce

---
+++ STL Neighborhood Guesser/Services/INeighborhoodService.cs
@@ -0,0 +1,21 @@
+﻿using STL_Neighborhood_Guesser.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace STL_Neighborhood_Guesser.Services
+{
+
+    public interface INeighborhoodService
+    {
+        public List<string> GetAll();
+
+        public List<Neighborhood> GetHintNeighborhoods();
+
+        public string ProcessGuess(double lon, double lat);
+
+        public void IncrementScore(IQueryable<Score> scores);
+
+        public void IncrementAttempts(IQueryable<Score> scores);
+
+        public Score ProcessScore();
+    }
--- STL Neighborhood Guesser/Services/NeighborhoodService.cs
+++ STL Neighborhood Guesser/Services/NeighborhoodService.cs
@@ -143,18 +143,4 @@ namespace STL_Neighborhood_Guesser.Services
 
     }
 
-    public interface INeighborhoodService
-    {
-        public List<string> GetAll();
-
-        public List<Neighborhood> GetHintNeighborhoods();
-
-        public string ProcessGuess(double lon, double lat);
-
-        public void IncrementScore(IQueryable<Score> scores);
-
-        public void IncrementAttempts(IQueryable<Score> scores);
-
-        public Score ProcessScore();
-    }
 }
